b03726c2d97efddb9e50aabe194d8b9df66049ca
Godwin Mafireyi <mafireyi@gmail.com>
2015-03-12 13:48:09 +0200

    my count_all_seqs assignment complete

--- bin/count_all_seqs.py
+++ bin/count_all_seqs.py
@@ -1,1 +1,18 @@
-# An empty file
+#!/usr/bin/env python
+
+import sys
+import os
+import seq_utils
+import os.path
+
+if len(sys.argv) < 2:
+        exit ('gimme a sequence')
+
+direc = sys.argv[1]
+for sequences in os.listdir(direc):
+        if sequences.endswith('.fasta'):
+                seq = open(os.path.join(direc,sequences))
+                seq_count = seq_utils.count_seqs(seq)
+                print sequences, seq_count
+
+
--- lib/seq_utils.py
+++ lib/seq_utils.py
@@ -1,1 +1,11 @@
-# An empty file
+#! usr/bin/env python
+
+def count_seqs(seq):
+    count = 0
+    for line in seq:
+        line = line.lstrip() # strip leading spaces, if any
+        if line.startswith('>'):
+            count += 1
+    return count
+
+
